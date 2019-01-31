2b71fa0bfac1e109a18997171818c3cc2e62f8e8
Francis Bui <witzp3ych0@gmail.com>
2019-01-31 01:13:05 -0500

    confirm password and account checker.

---
+++ Bank/SHA256.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <stdio.h>
+#include <time.h>       /* time */
+#include <stdlib.h>     /* srand, rand */\
+
+
+int main()
+{
+	int encryptionkey;
+
+	/* initialize random seed: */
+	srand(time(NULL));
+}
